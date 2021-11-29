0df6a5c59d3fb1c971ffc7bf032d7aa6b0a12aa6
Saleem Hussein <tug99306@temple.edu>
2021-11-29 16:48:23 -0500

    Add XML service

--- FurnitureStoreWebAPI/Startup.cs
+++ FurnitureStoreWebAPI/Startup.cs
@@ -25,7 +25,9 @@ namespace FurnitureStoreWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .AddXmlSerializerFormatters()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
