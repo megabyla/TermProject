3f571e42b415a5dc866dfc082f94135ce05e622e
Saleem Hussein <tug99306@temple.edu>
2021-11-30 01:20:20 -0500

    Reservation Model implementation

--- FurnitureStoreWebAPI/Models/Reservation.cs
+++ FurnitureStoreWebAPI/Models/Reservation.cs
@@ -7,6 +7,28 @@ namespace FurnitureStoreWebAPI.Models
 {
     public class Reservation
     {
+        public int ReservationId;
+        public String ReservationName;
+        public String ReservationTime;
+        public DateTime ReservationDate;
+        public int ReservationCount;
+        public int FurnitureId;
 
-    }
+
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int id, String name, String time,
+            DateTime date, int count, int furnitureId)
+        {
+            this.ReservationId = id;
+            this.ReservationName = name;
+            this.ReservationTime = time;
+            this.ReservationDate = date;
+            this.ReservationCount = count;
+            this.FurnitureId = furnitureId;
+        }
+}
 }
