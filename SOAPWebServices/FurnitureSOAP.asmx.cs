86554308a3252047ea2c73e3c1de00ac8ca37203
Nyla Rashied <tuh14505@temple.edu>
2021-12-01 12:33:56 -0500

    Created Furniture SOAP web service

---
+++ SOAPWebServices/FurnitureSOAP.asmx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SOAPWebServices
+{
+    /// <summary>
+    /// Summary description for Furniture
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class FurnitureSOAP : System.Web.Services.WebService
+    {
+
+       
+    }
+}
