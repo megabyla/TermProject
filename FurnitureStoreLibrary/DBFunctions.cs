0a4776984a50c65c14e3f9e19a1ecb1312de2782
Saleem Hussein <tug99306@temple.edu>
2021-12-01 23:41:58 -0500

    Make getfurniture dbfunction

--- FurnitureStoreLibrary/DBFunctions.cs
+++ FurnitureStoreLibrary/DBFunctions.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities;
 
 namespace FurnitureStoreLibrary
 {
     class DBFunctions
     {
+        public DataSet GetFurniture(DBConnect newDB)
+        {
+            SqlCommand objCommand = new SqlCommand();
+            DataSet dsFurniture;
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetFurniture";
+            dsFurniture = newDB.GetDataSetUsingCmdObj(objCommand);
+            return dsFurniture;
+        }
     }
 }
