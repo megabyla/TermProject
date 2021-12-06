4d91a8d6ddead4be443879191f55687b57fd38f3
Saleem Hussein <tug99306@temple.edu>
2021-12-06 12:47:34 -0500

    Update filter implementation with search bar

--- FurnitureStore/SearchFilter.ascx
+++ FurnitureStore/SearchFilter.ascx
@@ -1,1 +1,14 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SearchFilter.ascx.cs" Inherits="FurnitureStore.SearchFilter" %>
+<asp:Label ID="lblSearch" runat="server" Text="Search: "></asp:Label>
+&nbsp;&nbsp;&nbsp;
+<asp:TextBox ID="txtSearch" runat="server" OnTextChanged="txtSearch_TextChanged"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<asp:Label ID="lblFilter" runat="server" Text="Filter: "></asp:Label>
+&nbsp;&nbsp;&nbsp;
+<asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+    <asp:ListItem Value="null">Select a Type</asp:ListItem>
+    <asp:ListItem>Bedroom</asp:ListItem>
+    <asp:ListItem>Living Room</asp:ListItem>
+    <asp:ListItem>Dining Room</asp:ListItem>
+</asp:DropDownList>
+
--- FurnitureStore/SearchFilter.ascx.cs
+++ FurnitureStore/SearchFilter.ascx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,43 @@ namespace FurnitureStore
 {
     public partial class SearchFilter : System.Web.UI.UserControl
     {
+        int furnitureId;
+        string searchTerm;
+        string filterDdl;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        [Category("Misc")]
+        public int FurnitureId
+        {
+            get { return furnitureId; }
+            set { furnitureId = value; }
+        }
+
+        [Category("Misc")]
+        public string SearchTerm
+        {
+            get { return searchTerm; }
+            set { searchTerm = value; }
+        }
+
+        [Category("Misc")]
+        public string FilterDdl
+        {
+            get { return filterDdl; }
+            set { filterDdl = value; }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filterDdl = DropDownList1.SelectedItem.Value;
+        }
+
+        protected void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            searchTerm = txtSearch.Text;
         }
     }
 }
